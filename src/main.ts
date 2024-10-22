e0a6757812be7634907ad952c16453955b97ec55
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-10-22 14:45:10 -0700

    wadada

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ const ctx = canvas.getContext("2d")!;
 ctx.fillStyle = "black";
 ctx.fillRect(10, 10, 256, 256);
 ctx.fillStyle = "white";
-ctx.fillRect(16, 16, 250, 250);
+ctx.fillRect(15, 15, 246, 246);
 
 
