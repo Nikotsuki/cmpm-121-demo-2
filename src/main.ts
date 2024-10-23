5c1fb627afb53148147b4036f7b65dc12914eb6d
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-10-22 23:59:07 -0700

    b

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ const ctx = canvas.getContext("2d")!;
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, 266, 266);
 ctx.fillStyle = "white";
-ctx.fillRect(8, 8, 256, 256);
+ctx.fillRect(4, 4, 256, 256);
 
 
