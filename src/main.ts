5912e9af5e576d636b34aa5590b71d3ecb3b06f1
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-10-23 00:28:04 -0700

    step 2

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 header.innerHTML = APP_NAME;
 app.append(header);
 
+let isDrawing = false;
+let x = 0;
+let y = 0;
 
 const canvas: HTMLCanvasElement = document.querySelector("#canvas")!;
 const ctx = canvas.getContext("2d")!;
@@ -16,3 +19,49 @@ ctx.fillStyle = "white";
 ctx.fillRect(5, 5, 256, 256);
 
 
+// event.offsetX, event.offsetY gives the (x,y) offset from the edge of the canvas.
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+canvas.addEventListener("mousedown", (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+    isDrawing = true;
+});
+  
+canvas.addEventListener("mousemove", (e) => {
+    if (isDrawing) {
+      ctx.beginPath();
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 1;
+      ctx.moveTo(x, y);
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+      ctx.closePath();
+      x = e.offsetX;
+      y = e.offsetY;
+    }
+});
+  
+document.addEventListener("mouseup", (e) => {
+    if (isDrawing) {
+      ctx.beginPath();
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 1;
+      ctx.moveTo(x, y);
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+      ctx.closePath();
+      x = 0;
+      y = 0;
+      isDrawing = false;
+    }
+});
+  
+
+
+
+const clear: HTMLButtonElement = document.querySelector("#clear")!;
+clear.addEventListener("click", () => {
+    ctx.fillStyle = "white";
+    ctx.fillRect(5, 5, 256, 256);
+});
