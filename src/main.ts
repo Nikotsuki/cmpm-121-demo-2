93acafca6d2aa21300ac302f977ff562a6070c24
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-10-24 14:33:43 -0700

    Step 2 Complete

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,55 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 header.innerHTML = APP_NAME;
 app.append(header);
 
+let isDrawing = false;
+let x = 0;
+let y = 0;
 
 const canvas: HTMLCanvasElement = document.querySelector("#canvas")!;
 const ctx = canvas.getContext("2d")!;
 ctx.fillStyle = "white";
 ctx.fillRect(5, 5, 256, 256);
+
+canvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    ctx.moveTo(x, y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    ctx.closePath();
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+document.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    ctx.beginPath();
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 1;
+    ctx.moveTo(x, y);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    ctx.closePath();
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+
+
+
+const clear: HTMLButtonElement = document.querySelector("#clear")!;
+clear.addEventListener("click", () => {
+  ctx.fillStyle = "white";
+  ctx.fillRect(5, 5, 256, 256);
+});
