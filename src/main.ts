8d234ca77a9f2124be12b45370cd4674576acaf9
Nikotsuki <95511698+Nikotsuki@users.noreply.github.com>
2024-10-28 13:30:02 -0700

    Step 6 Complete

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ type Point = { x: number, y: number };
 let lines: Marker_line[] = []; 
 let currentLine: Marker_line;
 let redo_stack: Marker_line[] = [];
+let thickness: number = 1;
 
 interface Displayable {
   display(context: CanvasRenderingContext2D): void;
@@ -23,7 +24,7 @@ interface Displayable {
 
 
 function handleMouseDown(event: MouseEvent) {
-  currentLine = new Marker_line(event.offsetX, event.offsetY);
+  currentLine = new Marker_line(event.offsetX, event.offsetY, thickness);
 }
 
 
@@ -91,13 +92,27 @@ redo.addEventListener("click", () => {
   }
 });
 
+// thick button
+const thick: HTMLButtonElement = document.querySelector("#thick")!;
+thick.addEventListener("click", () => {
+  thickness = 6;
+});
+
+// thin button
+const thin: HTMLButtonElement = document.querySelector("#thin")!;
+thin.addEventListener("click", () => {
+  thickness = 1;
+});
+
 // Marker Class
 class Marker_line implements Displayable{
 
   public line: Point[] = [];
+  public marker_thickness: number;
 
-  constructor(init_x: number, init_y: number) {
+  constructor(init_x: number, init_y: number, _thickness: number) {
     this.line.push({x: init_x, y: init_y});
+    this.marker_thickness = _thickness;
   }
   
   public drag(x: number, y: number){
@@ -112,7 +127,7 @@ class Marker_line implements Displayable{
         ctx.lineTo(point.x, point.y);
     }
     ctx.strokeStyle = 'black';
-    ctx.lineWidth = 1;
+    ctx.lineWidth = this.marker_thickness;
     ctx.stroke();
     ctx.closePath();
   }
